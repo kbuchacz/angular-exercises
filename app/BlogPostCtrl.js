b62917b50db6a797ed0a57a067c01027a3f16c89
Krzysztof Buchacz <k.buchacz@gmail.com>
2014-09-22 19:07:01 +0200

    Display sample array

--- app/BlogPostCtrl.js
+++ app/BlogPostCtrl.js
@@ -1,7 +1,13 @@
 (function () {
     'use strict';
+    var example = {
+      id: 1,
+      name: "example",
+      pets: ["Dog1", "Cat1"]
+    };
+    
     function BlogPostCtrl() {
-
+      this.posts = [example];
     }
 
     var module = angular.module("exerciseApp", []);
