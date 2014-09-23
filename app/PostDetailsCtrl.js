4c4cae64b0a452eb93fbd68c27acb3cf44ee1109
Krzysztof Buchacz <k.buchacz@gmail.com>
2014-09-23 10:39:59 +0200

    Show data and route to details

--- app/PostDetailsCtrl.js
+++ app/PostDetailsCtrl.js
@@ -1,9 +1,15 @@
 (function () {
     'use strict';
-    function PostDetailsCtrl(PostDAO) {
+    function PostDetailsCtrl(PostDAO, $routeParams) {
+      this.details = {};
+      
+      var detailsScope = this;
+      PostDAO.get($routeParams.id).then(function(data) {
+        detailsScope.details = data;
+      });
 
     }
 
     var module = angular.module("exerciseApp");
-    module.controller('PostDetailsCtrl', ['PostDAO',PostDetailsCtrl]);
+    module.controller('PostDetailsCtrl', ['PostDAO', '$routeParams',PostDetailsCtrl]);
 })();
