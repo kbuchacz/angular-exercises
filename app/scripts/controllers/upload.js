00cad2f61671049d803d1f3d991ab5b7610cc674
Krzysztof Buchacz <k.buchacz@gmail.com>
2014-09-24 23:53:01 +0200

    Upload files and add image thumbnails

--- app/scripts/controllers/upload.js
+++ app/scripts/controllers/upload.js
@@ -8,21 +8,29 @@
  * Controller of the ngflowApp
  */
 angular.module('ngflowApp').controller('uploadCtrl', function ($scope) {
-
+    var ctrl = this;
     this.flowCompleteFlag = false;
 
     this.image = undefined;
+    this.file = undefined;
 
     this.startUpload = function(flow,files){
-	// write function body
-    }
+      flow.upload();
+    };
 
     this.fileSuccess = function(file,message){
-	// write function body
-    }
+      ctrl.image = JSON.parse(message);
+    };
+    
+    this.showImage = function(file) {
+      if(file.file.type.search('image') !== -1) {
+        return file;
+      }
+      return undefined;
+    };
 
     this.flowComplete=function(){
         this.flowCompleteFlag = true;
-    }
+    };
 
 });
